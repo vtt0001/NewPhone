3033d2331c5f3f83fbc8285df0f5b385b2ee8f54
VICTOR <vtt0001@correo.ugr.es>
2021-10-19 21:46:09 +0200

    Creada la estructura principal de la  clase modelo_tel close #7

---
+++ src/modelo_tel.rs
@@ -0,0 +1,10 @@
+//TODO Aún por definir
+#[derive(Debug)]
+pub struct Caracteristicas;
+
+#[derive(Debug)]
+pub struct ModeloTel{
+    pub marca: String,
+    pub modelo: String,
+    pub caracteristicas: Caracteristicas,
+}
