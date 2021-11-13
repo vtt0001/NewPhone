353a572cb0f93873929a9b36cc3943542378aa7e
VICTOR <vtt0001@correo.ugr.es>
2021-11-13 13:00:39 +0100

    Pasamos a gren/refactor el test unitario test_nuevas_caracteristicas #17

--- src/caracteristicas.rs
+++ src/caracteristicas.rs
@@ -27,10 +27,26 @@ pub struct Caracteristicas{
 }
 
 impl Caracteristicas{
-    pub fn new(new_camara_frontal: u16, new_camaras_traseras: Vec<u16>, new_ram: u16, new_rom: u16, 
-        new_memoria_interna: u16, vel_procesador: f32, new_dimensiones: [f32; 3], new_peso: f32, 
-        new_capacidad_bateria: u32, new_cinco_g: bool, new_nfc: bool, new_infrarrojos: bool, new_gps: bool,
-    new_lector_huella: bool, new_dual_sim: bool, new_bluetooth: bool, new_num_nucleos: u8, new_tam_pantalla: f32,) -> Caracteristicas{
-        panic!();
+    pub fn new(new_camara_frontal: u16, new_camaras_traseras: Vec<u16>, new_ram: u16, new_rom: u16, new_memoria_interna: u16, new_vel_procesador: f32, new_dimensiones: [f32; 3], new_peso: f32, new_capacidad_bateria: u32, new_cinco_g: bool, new_nfc: bool, new_infrarrojos: bool, new_gps: bool, new_lector_huella: bool, new_dual_sim: bool, new_bluetooth: bool, new_num_nucleos: u8, new_tam_pantalla: f32,) -> Caracteristicas{
+        Caracteristicas{
+            camara_frontal:new_camara_frontal,
+            camaras_traseras:new_camaras_traseras,
+            ram:new_ram,
+            rom:new_rom,
+            memoria_interna:new_memoria_interna,
+            vel_procesador:new_vel_procesador,
+            dimensiones:new_dimensiones,
+            peso:new_peso,
+            capacidad_bateria:new_capacidad_bateria,
+            cinco_g:new_cinco_g,
+            nfc:new_nfc,
+            infrarrojos:new_infrarrojos,
+            gps:new_gps,
+            lector_huella:new_lector_huella,
+            dual_sim:new_dual_sim,
+            bluetooth:new_bluetooth,
+            num_nucleos:new_num_nucleos,
+            tam_pantalla:new_tam_pantalla,
+        }
     }
 }
