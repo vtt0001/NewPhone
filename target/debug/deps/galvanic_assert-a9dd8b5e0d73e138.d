42b2b7f6664133dd6461eed2b6de5eb4dd17afc6
VICTOR <vtt0001@correo.ugr.es>
2021-11-25 21:34:24 +0100

    [akip ci] eliminamos taget de .gitignore. close #39

---
+++ target/debug/deps/galvanic_assert-a9dd8b5e0d73e138.d
@@ -0,0 +1,10 @@
+/home/victor/Escritorio/Master/2.Primer Cuatri/CC/NewPhone/target/debug/deps/galvanic_assert-a9dd8b5e0d73e138.rmeta: /home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/lib.rs /home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/matchers/mod.rs /home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/matchers/core.rs /home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/matchers/combinators.rs /home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/matchers/variant.rs /home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/matchers/collection.rs
+
+/home/victor/Escritorio/Master/2.Primer Cuatri/CC/NewPhone/target/debug/deps/galvanic_assert-a9dd8b5e0d73e138.d: /home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/lib.rs /home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/matchers/mod.rs /home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/matchers/core.rs /home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/matchers/combinators.rs /home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/matchers/variant.rs /home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/matchers/collection.rs
+
+/home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/lib.rs:
+/home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/matchers/mod.rs:
+/home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/matchers/core.rs:
+/home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/matchers/combinators.rs:
+/home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/matchers/variant.rs:
+/home/victor/.cargo/registry/src/github.com-1ecc6299db9ec823/galvanic-assert-0.8.7/src/matchers/collection.rs:
---
+++ target/debug/deps/newphone-30ceb6d915af8356.d
@@ -0,0 +1,8 @@
+/home/victor/Escritorio/Master/2.Primer Cuatri/CC/NewPhone/target/debug/deps/newphone-30ceb6d915af8356: src/main.rs src/caracteristicas.rs src/modelo_tel.rs src/test_modelo_tel.rs
+
+/home/victor/Escritorio/Master/2.Primer Cuatri/CC/NewPhone/target/debug/deps/newphone-30ceb6d915af8356.d: src/main.rs src/caracteristicas.rs src/modelo_tel.rs src/test_modelo_tel.rs
+
+src/main.rs:
+src/caracteristicas.rs:
+src/modelo_tel.rs:
+src/test_modelo_tel.rs:
