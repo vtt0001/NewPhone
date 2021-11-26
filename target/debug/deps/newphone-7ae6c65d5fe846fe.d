4193fb3aa71e3bff4829b0b39177663817d1d065
VICTOR <vtt0001@correo.ugr.es>
2021-11-26 12:52:30 +0100

    Incluir target en .gitignore. #42

--- target/debug/deps/newphone-7ae6c65d5fe846fe.d
+++ target/debug/deps/newphone-7ae6c65d5fe846fe.d
@@ -1,6 +1,6 @@
-/home/victor/Escritorio/Master/2.Primer Cuatri/CC/NewPhone/target/debug/deps/newphone-7ae6c65d5fe846fe: src/main.rs src/caracteristicas.rs src/modelo_tel.rs src/test_modelo_tel.rs
+/app/test/target/debug/deps/newphone-7ae6c65d5fe846fe: src/main.rs src/caracteristicas.rs src/modelo_tel.rs src/test_modelo_tel.rs
 
-/home/victor/Escritorio/Master/2.Primer Cuatri/CC/NewPhone/target/debug/deps/newphone-7ae6c65d5fe846fe.d: src/main.rs src/caracteristicas.rs src/modelo_tel.rs src/test_modelo_tel.rs
+/app/test/target/debug/deps/newphone-7ae6c65d5fe846fe.d: src/main.rs src/caracteristicas.rs src/modelo_tel.rs src/test_modelo_tel.rs
 
 src/main.rs:
 src/caracteristicas.rs:
